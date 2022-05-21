98ffa921c89e5a7ea8569343644316746ad2261d
vexleet <abda70226@gmail.com>
2022-05-21 20:48:20 +0200

    initital commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          700: '#383836',
+          900: '#2C2A28'
+        },
+        secondary: '#FEED01'
+      }
+    },
+
+  },
+  plugins: [],
+}
